acb4661e44962300663eb602e4cd40bf56604158
biologist01 <urwashaikh01@gmail.com>
2025-12-10 14:36:05 +0000

    deploy: 104e4b3127807a12615b28bdda37bb9dbf358288

---
+++ assets/js/aba21aa0.6399903f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkplant_biotech_ai=self.webpackChunkplant_biotech_ai||[]).push([[5742],{7093:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
---
+++ assets/js/cced1b58.49506d1c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkplant_biotech_ai=self.webpackChunkplant_biotech_ai||[]).push([[6100],{5852:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"docs-urdu"}')}}]);
